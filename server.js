aece27160aefdaac5e4b2b1c795b333a449ca43f
tondi <mtondos2@gmail.com>
2017-05-18 12:16:22 +0200

    added socket.io integration with adding, direction and color

--- server.js
+++ server.js
@@ -7,21 +7,41 @@ var http = require('http').Server(app);
 // })
 
 var server = app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
+    console.log('Port 3000!')
 })
 var io = require('socket.io').listen(server);
 
 io.on('connection', function(socket) {
-  console.log('a user connected');
+    console.log('a user connected');
 
-  socket.on("data/scene-children", function(data) {
-    console.log("odbieranie data scene")
-    console.log(data)
-  })
+    socket.on("block/add", data => {
+        console.log(data)
+        socket.broadcast.emit("block/add", data)
+    })
+    socket.on("block/change-color", data => {
+        console.log(data)
+        socket.broadcast.emit("block/change-color", data)
+    })
 
-  socket.on("disconnect", function() {
-    console.log("klient się rozłącza")
-  })
+    socket.on("block/change-size", data => {
+        console.log(data)
+            // io.sockets.emit("block/change-size", data)
+        socket.broadcast.emit("block/change-size", data)
+    })
+
+    socket.on("block/change-rotation", data => {
+        console.log(data)
+        socket.broadcast.emit("block/change-rotation", data)
+    })
+
+    // socket.on("data/scene-children", function(data) {
+    //     console.log("odbieranie data scene")
+    //     console.log(data)
+    // })
+
+    socket.on("disconnect", function() {
+        console.log("klient się rozłącza")
+    })
 });
 
 app.use(express.static('static'))
--- static/js/Network.js
+++ static/js/Network.js
@@ -1,11 +1,83 @@
+// {
+//     "_id":"58ec9ffcf2218f23d8f38125"
+//     "login":"bbb"
+//     "building":[
+//     {
+//         "rot": 0,
+//         "z": -250,
+//         "y": 0,
+//         "x": 50,
+//         "color": 2,
+//         "num": 1
+//     },
+//     {
+//         "rot": 0,
+//         "z": -250,
+//         "y": 50,
+//         "x": 50,
+//         "color": 2,
+//         "num": 1
+//     }
+//     ]
+
+// }
+
+// TODO: Clean up this fucking mess in Game.js legacy functions 
 function Network() {
-  // this.test = "x"
-  var socket = io();
-  var sceneData = main.scene.toJSON();
+    // this.test = "x"
+    var socket = io();
 
-  this.publishObjects = function() {
+    this.addBlock = function(block) {
+            var blockInfo = {
+                rot: block.rotation,
+                x: block.position.x,
+                y: block.position.y,
+                z: block.position.z,
+                // temporarily
+                color: block.children[0].material.color,
+                num: 1
+            }
 
-    console.log(sceneData)
-    socket.emit("data/scene-children", { data: sceneData })
-  }
+            // send to server
+            socket.emit("block/add", blockInfo)
+
+            // recieve from server
+        }
+        // socket.broadcast.to('game').emit('message', 'nice game');
+    socket.on("block/add", data => {
+        game.addLegacyBlock(data)
+    })
+
+    //  ------- rozmiar
+    this.changeBlockSize = function(direction) {
+        socket.emit("block/change-size", direction)
+    }
+    socket.on("block/change-size", data => {
+        game.changeLegacyBlockSize(data)
+    })
+
+    // ------- color
+    this.changeBlockColor = function(color) {
+        socket.emit("block/change-color", color)
+    }
+    socket.on("block/change-color", data => {
+        game.changeLegacyBlockColor(data)
+    })
+
+    // ------- rotation
+    this.changeBlockRotation = function(rotation) {
+        socket.emit("block/change-rotation", rotation)
+    }
+    socket.on("block/change-rotation", data => {
+        game.changeLegacyBlockRotation(data)
+    })
+
+
+    // var sceneData = main.scene.toJSON();
+
+    // this.publishObjects = function() {
+
+    //     console.log(sceneData)
+    //     socket.emit("data/scene-children", { data: sceneData })
+    // }
 }
